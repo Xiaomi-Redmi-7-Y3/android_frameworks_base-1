7a69bb0b9656793c20a1c8a58da9d1bcaa33e88a
TikkiTikki <radstylemusic@gmail.com>
2021-05-02 18:04:37 -0300

    keyguard: clocks: Add Android S clock
    LordShenron: This commit only adds AndroidS clock and not AndroidDP3 clock. Why? Because only Android S clock is something different.
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Change-Id: I10949ef755382c8382aa338add03913349841116

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -154,6 +154,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AndroidSDP3ClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new AndroidSClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
