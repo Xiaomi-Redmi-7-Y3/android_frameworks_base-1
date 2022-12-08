f0aa74d3c540a9e259534b8a0bae00901e136f1d
Galitsky Dmytro <melles.android@gmail.com>
2022-12-08 08:43:06 +0200

    Update ExodusUtils.java

--- core/java/com/android/internal/util/exodus/ExodusUtils.java
+++ core/java/com/android/internal/util/exodus/ExodusUtils.java
@@ -46,7 +46,7 @@ public class ExodusUtils {
                 }
             }
         } catch (CameraAccessException e) {
-             Ignore
+            // Ignore
         }
         return false;
     }
