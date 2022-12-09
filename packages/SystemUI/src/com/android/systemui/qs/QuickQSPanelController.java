b16683a185dc6b11b1f4538da084f045f31b24c4
Sensei-Developer <111665604+Sensei-Developer@users.noreply.github.com>
2022-12-09 10:52:05 +0300

    fixup3! SystemUI: Add QS tile layout settings [1/2]

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
@@ -31,6 +31,7 @@ import com.android.systemui.qs.customize.QSCustomizerController;
 import com.android.systemui.qs.dagger.QSScope;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.TileUtils;
+import com.android.systemui.tuner.TunerService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,6 +54,7 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
             };
 
     private final FooterActionsController mFooterActionsController;
+    private final TunerService mTunerService;
 
     @Inject
     QuickQSPanelController(QuickQSPanel view, QSTileHost qsTileHost,
@@ -61,10 +63,12 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
             @Named(QUICK_QS_PANEL) MediaHost mediaHost,
             MetricsLogger metricsLogger, UiEventLogger uiEventLogger, QSLogger qsLogger,
             DumpManager dumpManager,
-            @Named(QQS_FOOTER) FooterActionsController footerActionsController
+            @Named(QQS_FOOTER) FooterActionsController footerActionsController,
+            TunerService tunerService
     ) {
         super(view, qsTileHost, qsCustomizerController, usingMediaPlayer, mediaHost, metricsLogger,
                 uiEventLogger, qsLogger, dumpManager, tunerService);
+        mTunerService = tunerService;
         mFooterActionsController = footerActionsController;
     }
 
